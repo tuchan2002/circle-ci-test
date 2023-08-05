6f99d55a7575e23c302ee369e039e39c1042ad06
tuchan2002 <trananhtu171002@gmail.com>
2023-08-05 14:46:28 +0700

    push

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Worl</p>
+        <p>Hello World</p>
       </header>
     </div>
   );
