9777071f16865d4473fcf5b0c2179bf801957cdd
tuchan2002 <trananhtu171002@gmail.com>
2023-08-05 22:29:37 +0700

    fix code

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import logo from './logo.svg';
-import './App.css'
+import './App.css';
 
 function App() {
     return (
         <div className='App'>
             <header className='App-header'>
                 <img src={logo} className='App-logo' alt='logo' />
-                <p>Hello Wold !</p>
+                <p>Hello World</p>
             </header>
         </div>
     );
