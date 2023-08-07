56423a71a66d21718873f5921826fd5070537398
tuchan2002 <trananhtu171002@gmail.com>
2023-08-07 23:44:13 +0700

    add deploy job

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
             <header className='App-header'>
                 <img src={logo} className='App-logo' alt='logo' />
                 <p>Hello World</p>
+                <a href="https://www.youtube.com/channel/UCL5vFihmuBo1D3A763gA5Gg">Youtube Channel</a>
             </header>
         </div>
     );
