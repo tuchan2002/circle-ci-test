df8fd75bf0feaec3dd9d69cb1e4dca8921440862
tuchan2002 <trananhtu171002@gmail.com>
2023-08-09 17:17:17 +0700

    push code

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
             <header className='App-header'>
                 <img src={logo} className='App-logo' alt='logo' />
                 <p>Hello World !</p>
-                <a href="https://www.youtube.com/channel/UCL5vFihmuBo1D3A763gA5Gg">Youtube Channel</a>
+                <p>Copyright by <a href="https://www.youtube.com/channel/UCL5vFihmuBo1D3A763gA5Gg">TuChan</a></p>
             </header>
         </div>
     );
